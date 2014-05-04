fef1cadf76429292736aa0cfe3ebcf75752b8fc8
Oleksandr Sabov <alexsabov@gmail.com>
2014-05-04 22:45:02 +0200

    Add publishedIn func

--- assignment02/exercise1.hs
+++ assignment02/exercise1.hs
@@ -24,4 +24,19 @@ treeToList (Node x) = foldl (\acc el -> case el of
                                 (subTree, _) -> acc ++ treeToList subTree
                             ) [] x
 
-main = print $ treeToList myTree
+contains :: Ord a => a -> IndexedTree a -> Bool
+contains num (Leaf x) = elem num x
+contains num (Node x) = forEachTwoElements (\a b -> case a of
+                                                (subTree1, index1) -> case b of
+                                                    (subTree2, index2) -> if num >= index1 && num < index2
+                                                                then contains num subTree1
+                                                                else False
+                                           ) x
+
+
+forEachTwoElements :: (a -> a -> b) -> [a] -> [b]
+forEachTwoElements fn [] = []
+forEachTwoElements fn (x:[]) = []
+forEachTwoElements fn (x:xs:xss) = fn x xs : forEachTwoElements fn (xs:xss)
+
+main = print $ contains 22 myTree
---
+++ assignment02/exercise3.hs
@@ -0,0 +1,33 @@
+data Genre = Nonfiction | Novel | Biography deriving (Eq, Show)
+type Name = (String, String)
+type Date = (Int, Int, Int) -- day, month, year
+data Book = ABook Genre
+                  Name   -- name of the author
+                  String -- title of the book
+                  Date   -- date of publication
+                  Int    -- number of pages
+            deriving Show
+
+genre :: Book -> Genre
+genre (ABook g _ _ _ _) = g
+author :: Book -> Name
+author (ABook _ a _ _ _) = a
+title :: Book -> String
+title (ABook _ _ t _ _) = t
+date :: Book -> Date
+date (ABook _ _ _ d _) = d
+pages :: Book -> Int
+pages (ABook _ _ _ _ p) = p
+year :: Book -> Int
+year b = let (_, _, y) = date b in y
+
+breakingNews = ABook Novel ("Schaetzing", "Frank") "Breaking News" (06, 03, 2014) 976
+snowden = ABook Nonfiction ("Harding", "Luke") "The Snowden Files" (06, 02, 2014) 346
+futureShock = ABook Nonfiction ("Toffler", "Alvin") "Future Shock" (01, 06, 1984) 576
+
+publishedIn :: Int -> [Book] -> [Book]
+publishedIn year books = filter (\book -> case date book of
+                                    (_, _, currYear) -> year == currYear
+                                ) books
+
+main = print $ publishedIn 1984 [breakingNews, snowden, futureShock]
