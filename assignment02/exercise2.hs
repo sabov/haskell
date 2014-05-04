d8a9748a8d0964a2ecdabab87b7006e4de60a840
Peetwriter <pereter92@gmail.com>
2014-05-04 22:43:15 +0200

    exercise2 finalized

--- assignment02/exercise2.hs
+++ assignment02/exercise2.hs
@@ -1,7 +1,14 @@
-data Regex = Symbol Char
-            | Empty
+data Regex =  Empty
+            | Symbol Char
+	    | Kleene Regex
             | Concat Regex Regex
             | Or Regex Regex
-            | Kleene Regex
-	    	
+            
+
+instance Show Regex where
+	show Empty = "{}"
+	show (Symbol s) = [s]
+	show (Kleene star) = "(" ++ show star ++ ")" ++ "*"
+	show (Concat l r) = show l ++ show r
+	show (Or l r) = "(" ++ show l ++ " | " ++ show r ++ ")" 
 
