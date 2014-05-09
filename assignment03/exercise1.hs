122a7a68ed566d6710925ec0283d2f01cb498141
Oleksandr Sabov <alexsabov@gmail.com>
2014-05-10 00:07:00 +0200

    Start to write cantor list function

--- assignment03/exercise1.hs
+++ assignment03/exercise1.hs
@@ -1,3 +1,7 @@
+import Data.Ratio
+
+-- Ex1
+
 type Pupil = String -- the pupil's name
 type Group = Int
 
@@ -7,4 +11,11 @@ count numberOfGroups = zip xs where xs = countList 1 numberOfGroups
 countList :: Int -> Int -> [Int]
 countList from to = [from..to] ++ countList from to
 
-main = print $ count 3 ["Petro", "Frank", "Sandra", "Caro", "Max"]
+-- Ex2
+
+cantorList :: Int -> Int -> [Int]
+cantorList sign to | sign > 0 = [1..to] ++ cantorList sign (to + 1)
+                   | otherwise = reverse [1..to] ++ cantorList sign (to + 1)
+
+--main = print $ count 3 ["Petro", "Frank", "Sandra", "Caro", "Max"]
+main = print $ take 10 $ zip (cantorList 1 1) (cantorList (-1) 1)
